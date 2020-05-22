96d3f8ad2039b7cce32d2cc46a69bf26e652c4a0
Harry Turnbull <hturnbull@live.co.uk>
2020-05-22 11:30:37 +0100

    extract pounds and pence methods to money module

--- lib/account.py
+++ lib/account.py
@@ -1,14 +1,11 @@
+from . import money
+
 class Account:
     STARTING_BALANCE = 0
     balance = STARTING_BALANCE
     def deposit(self, amount):
-        credit = self.pence(amount)
+        credit = money.pence(amount)
         self.balance += credit
         message = "{0:.2f} deposited. Current balance: {1:.2f}"
-        return message.format(self.pounds(credit), self.pounds(self.balance))
+        return message.format(money.pounds(credit), money.pounds(self.balance))
 
-    def pence(self, pounds):
-        return pounds * 100
-
-    def pounds(self, pence):
-        return pence / 100
---
+++ lib/money.py
@@ -0,0 +1,5 @@
+def pence(pounds):
+    return pounds * 100
+
+def pounds(pence):
+    return pence / 100
