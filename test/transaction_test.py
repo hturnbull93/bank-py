58d5d3a4b936db7eae814731207d9a80491a5970
Harry Turnbull <hturnbull@live.co.uk>
2020-05-22 13:04:01 +0100

    readjust where time is mocked

--- test/transaction_test.py
+++ test/transaction_test.py
@@ -1,12 +1,12 @@
 from freezegun import freeze_time
 import datetime
 
-with freeze_time("2020-01-01"):
-    from lib.transaction import Transaction
+from lib.transaction import Transaction
 
 
 class TestDisplay():
     def test_has_date_formatted(self):
-        transaction = Transaction()
-        result = "01/01/2020 || || || "
-        assert transaction.display() == result
+        with freeze_time("2020-01-01"):
+            transaction = Transaction()
+            result = "01/01/2020 || || || "
+            assert transaction.display() == result
