f9ce7fe12600c8a989e9150498252f9b5eb53e37
Harry Turnbull <hturnbull@live.co.uk>
2020-05-22 12:55:22 +0100

    rework date stub

--- test/transaction_test.py
+++ test/transaction_test.py
@@ -1,12 +1,12 @@
 from freezegun import freeze_time
 import datetime
 
-from lib.transaction import Transaction
+with freeze_time("2020-01-01"):
+    from lib.transaction import Transaction
 
 
 class TestDisplay():
     def test_has_date_formatted(self):
-        with freeze_time("2020-01-01"):
-            transaction = Transaction()
-            result = "01/01/2020 || || || "
-            assert transaction.display() == result
+        transaction = Transaction()
+        result = "01/01/2020 || || || "
+        assert transaction.display() == result
